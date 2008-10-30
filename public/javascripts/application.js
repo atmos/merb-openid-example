d124f4e657db7ee3587dd759658aa161c8bfcf1b
atmos <atmos@atmos.org>
2008-10-30 02:24:58 -0600

    make for a better example when you launch the app

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,8 @@
 // Common JavaScript code across your application goes here.
+$(function() {
+  $("#navcontainer:has(li) ul").each(function() {
+    $(this).tabs();
+    var index = $("li[selected=selected] a", this).attr("href");
+    $(this).data("tabs").select(index);
+  });
+});
