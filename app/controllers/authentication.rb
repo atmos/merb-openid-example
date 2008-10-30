18852a2eec664ab850ac8294081ac51baf03db29
atmos <atmos@atmos.org>
2008-10-29 20:21:18 -0600

    handle random signup requests

--- app/controllers/authentication.rb
+++ app/controllers/authentication.rb
@@ -1,6 +1,6 @@
 class Authentication < Merb::Controller
   def signup
-    redirect!(url(:signin)) if session['openid.url'].nil?
+    return redirect(url(:login)) if session['openid.url'].nil?
     
     session.user = User.first_or_create({:identity_url => session['openid.url']},
                                 {:email => session['openid.email'], :name => session['openid.nickname']})
--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -7,39 +7,53 @@ describe Authentication do
                      :identity_url => 'http://foo.myopenid.com' }
     User.create(@user_params)
   end
-
+  
   describe "#signup" do
-    before(:each) do
-      @response = dispatch_to(Authentication, :signup) do |controller|
-        mock(controller.session).[](:user).times(3) { User.first.id }
-        mock(controller.session).[]('openid.url').twice { 'http://ceel0.myopenidizzle.com' }
-        mock(controller.session).[]('openid.email') { 'gangsters@ceelo.com' }
-        mock(controller.session).[]('openid.nickname') { 'ceelo' }
+    describe "a user with no session data" do
+      before(:each) do
+        @response = request(url(:signup), :method => 'GET')
+      end
+      it "should return http redirect" do
+        @response.status.should == 302
+      end
+      it "should redirect to the root user" do
+        @response.body.to_s.should match(%r!href="/login"!)
       end
     end
-    it "should return http redirect" do
-      @response.status.should == 302
-    end
-    it "should redirect to the root user" do
-      @response.body.should have_xpath("//a[@href='/users']")
-    end
-  end
-
-  describe "#signup" do
-    before(:each) do
-      @response = dispatch_to(Authentication, :signup) do |controller|
-        mock(controller.session).[](:user).times(3) { User.first.id }
-        mock(controller.session).[]('openid.url').twice { 'http://ceel0.myopenidizzle.com' }
-        mock(controller.session).[]('openid.email') { 'gangsters@ceelo.com' }
-        mock(controller.session).[]('openid.nickname') { 'atmos' }
+    describe "valid user data returned" do
+      before(:each) do
+        @response = dispatch_to(Authentication, :signup) do |controller|
+          mock(controller.session).[](:user).times(3) { User.first.id }
+          mock(controller.session).[]('openid.url').twice { 'http://ceel0.myopenidizzle.com' }
+          mock(controller.session).[]('openid.email') { 'gangsters@ceelo.com' }
+          mock(controller.session).[]('openid.nickname') { 'ceelo' }
+        end
+      end
+      it "should return http redirect" do
+        @response.status.should == 302
+      end
+      it "should redirect to the root user" do
+        @response.body.should have_xpath("//a[@href='/users']")
       end
     end
-    it "should return http redirect" do
-      @response.status.should == 302
+
+    describe "invalid user data returned" do
+      before(:each) do
+        @response = dispatch_to(Authentication, :signup) do |controller|
+          mock(controller.session).[](:user).times(3) { User.first.id }
+          mock(controller.session).[]('openid.url').twice { 'http://ceel0.myopenidizzle.com' }
+          mock(controller.session).[]('openid.email') { 'gangsters@ceelo.com' }
+          mock(controller.session).[]('openid.nickname') { 'atmos' }
+        end
+      end
+      it "should return http redirect" do
+        @response.status.should == 302
+      end
+      it "should redirect to the root user" do
+        @response.body.should have_xpath("//a[@href='/login']")
+      end
+      it "should give the user a message about validation errors"
     end
-    it "should redirect to the root user" do
-      @response.body.should have_xpath("//a[@href='/login']")
-    end
-    it "should give the user a message about validation errors"
+    
   end
 end
