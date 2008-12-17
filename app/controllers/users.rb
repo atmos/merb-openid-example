5169a2a388a814288d217f2bf200d3aa2bd7b80c
Corey Donohoe <atmos@atmos.org>
2008-12-16 23:26:39 -0700

    nuke code that's unused

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -2,64 +2,8 @@ class Users < Application
   # provides :xml, :yaml, :js
   before :ensure_authenticated
 
-  def index
-    @users = User.all
-    display @users
-  end
-
-  def show(id)
-    @user = User.get(id)
-    raise NotFound unless @user
-    display @user
-  end
-
-  def new
-    only_provides :html
-    @user = User.new
-    display @user
-  end
-
-  def edit(id)
-    only_provides :html
-    @user = User.get(id)
-    raise NotFound unless @user
-    display @user
-  end
-
-  def create(user)
-    @user = User.new(user)
-    if @user.save
-      redirect url(:user, @user.id), :message => {:notice => "User was successfully created"}
-    else
-      message[:error] = "User failed to be created"
-      render :new
-    end
-  end
-
-  def update(id, user)
-    @user = User.get(id)
-    raise NotFound unless @user
-    @user.update_attributes(user)
-    if @user.errors.empty?
-       redirect url(:user, @user.id), :message => {:notice => "User was successfully updated"}
-    else
-      display @user, :edit
-    end
-  end
-
-  def destroy(id)
-    @user = User.get(id)
-    raise NotFound unless @user
-    if @user.destroy
-      redirect resource(:users)
-    else
-      raise InternalServerError
-    end
-  end
-  
   def login
     # if the user is logged in, then redirect them to their profile
     redirect url(:user, session.user.id), :message => { :notice => 'You are now logged in' }
   end
-
 end # Users
