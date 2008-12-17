c618b78c1d85f12a6eb88d9050bfe20dc6fb3219
Corey Donohoe <atmos@atmos.org>
2008-12-17 00:33:03 -0700

    add the actions back that are actually used!

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -2,8 +2,20 @@ class Users < Application
   # provides :xml, :yaml, :js
   before :ensure_authenticated
 
+  def index
+    @users = User.all
+    display @users
+  end
+
+  def show(id)
+    @user = User.get(id)
+    raise NotFound unless @user
+    display @user
+  end
+
   def login
     # if the user is logged in, then redirect them to their profile
     redirect url(:user, session.user.id), :message => { :notice => 'You are now logged in' }
   end
-end # Users
+
+end
